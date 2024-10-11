1ec0e1a2783ce3bba33cb3e57564bf505de8463e
prove3305 <noname0520@naver.com>
2024-10-11 16:19:35 +0900

    6주차

---
+++ src/ArrayPractice.java
@@ -0,0 +1,9 @@
+public class ArrayPractice {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        int max = numbers[0];
+        for (int i = 0; i < numbers.length; i++) { //배열은 0부터 부조건
+            System.out.println(numbers[i]);
+        }
+    }
+}
---
+++ src/StringArray.java
@@ -0,0 +1,8 @@
+public class StringArray {
+    public static void main(String[] args) {
+        int[] number = {10, 30, 20, 50, 40};
+        String[] fruits = {"Apple", "Banana", "Orange"};
+
+
+    }
+}
