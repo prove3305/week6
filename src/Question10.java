29cdc0b4ba1d6c9192d5b4086fe6d049e55f7ea1
prove3305 <noname0520@naver.com>
2024-10-23 16:37:45 +0900

    9주차 숙제

--- src/Question10.java
+++ src/Question10.java
@@ -2,13 +2,12 @@ public class Question10 {
     public static void main(String[] args) {
         int[] num = {20, 30, 50, 70, 90};
         int sum = 0;
-        int result = 0;
 
         for(int i = 0; i < num.length; i++)
         {
             sum += num[i];
         }
-        result = sum/num.length;
+        double result = (double) sum / num.length;
         System.out.println("요소들의 평균: " + result);
     }
 }
